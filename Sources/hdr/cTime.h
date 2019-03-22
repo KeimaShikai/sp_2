01c7e7e24b8c369fefc2b609c7a5dd8f7df90c95
keimashikai <shikamaru740@gmail.com>
2019-03-22 16:05:06 +0700

    Add first tasks

--- Sources/hdr/cTime.h
+++ Sources/hdr/cTime.h
@@ -3,58 +3,71 @@
 #ifndef CTIME_H
 #define CTIME_H
 
-namespace myspace
+
+struct my_time
 {
+    short int hours, minutes;
 
-    struct time
-    {
-        short int hours, minutes;
+    my_time() {}
+    my_time(short int x, short int y) : hours(x), minutes(y) {}
+    my_time operator = (const my_time &t1);
+};
 
-        time() {}
-        time(short int x, short int y) : hours(x), minutes(y) {}
-        time operator = (const time &t1);
-    };
+const bool operator < (const my_time &t1, const my_time &t2)
+{
+    int ttemp1 = t1.hours * 60 + t1.minutes;
+    int ttemp2 = t2.hours * 60 + t1.minutes;
 
-    const bool operator < (const time &t1, const time &t2)
-    {
-        int ttemp1 = t1.hours * 60 + t1.minutes;
-        int ttemp2 = t2.hours * 60 + t1.minutes;
-
-        return ttemp1 < ttemp2;
-    }
-
-    const bool operator > (const time &t1, const time &t2)
-    {
-        int ttemp1 = t1.hours * 60 + t1.minutes;
-        int ttemp2 = t2.hours * 60 + t1.minutes;
-
-        return ttemp1 > ttemp2;
-    }
-
-    const bool operator == (const time &t1, const time &t2)
-    {
-        if ((t1.hours == t2.hours) && (t1.minutes == t2.minutes))
-            return true;
-        return false;
-    }
-
-    const bool operator != (const time &t1, const time &t2)
-    {
-        return !(t1 == t2);
-    }
-
-    time time::operator = (const time &t1)
-    {
-        if (&t1 == this)
-            return *this;
-        return time(this->hours = t1.hours, this->minutes = t1.minutes);
-    }
-
+    return ttemp1 < ttemp2;
 }
 
-void output(const myspace::time &t1)
+const bool operator > (const my_time &t1, const my_time &t2)
 {
-    std::cout << t1.hours << ":" << t1.minutes;
+    int ttemp1 = t1.hours * 60 + t1.minutes;
+    int ttemp2 = t2.hours * 60 + t1.minutes;
+
+    return ttemp1 > ttemp2;
+}
+
+const bool operator == (const my_time &t1, const my_time &t2)
+{
+    if ((t1.hours == t2.hours) && (t1.minutes == t2.minutes))
+        return true;
+    return false;
+}
+
+const bool operator != (const my_time &t1, const my_time &t2)
+{
+    return !(t1 == t2);
+}
+
+my_time my_time::operator = (const my_time &t1)
+{
+    if (&t1 == this)
+        return *this;
+    return my_time(this->hours = t1.hours, this->minutes = t1.minutes);
+}
+
+std::ostream& operator << (std::ostream &s, const my_time &t)
+{
+    s << '(' << t.hours << ":" << t.minutes << ")";
+    return s;
+}
+
+std::istream& operator >> (std::istream &s, my_time &t)
+{
+    std::cin >> t.hours >> t.minutes;
+    return s;
+}
+
+bool increase(my_time &t1, my_time &t2)
+{
+    return t1 > t2;
+}
+
+bool decrease(my_time &t1, my_time &t2)
+{
+    return t1 < t2;
 }
 
 #endif
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,8 +1,77 @@
 #include "cTime.h"
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+
+template <class T1>
+void sort_decrease(T1 &sample)
+{
+    std::vector<my_time> temp;
+    while (!sample.empty())
+    {
+        temp.emplace_back(sample.top());
+        sample.pop();
+    }
+    std::sort(temp.begin(), temp.end(), decrease);
+    while (!temp.empty())
+    {
+        sample.emplace(temp.back());
+        temp.pop_back();
+    }
+}
+
+template <class T1>
+void sort_increase(T1 &sample)
+{
+    std::vector<my_time> temp;
+    while (!sample.empty())
+    {
+        temp.emplace_back(sample.top());
+        sample.pop();
+    }
+    std::sort(temp.begin(), temp.end(), increase);
+    while (!temp.empty())
+    {
+        sample.emplace(temp.back());
+        temp.pop_back();
+    }
+}
+
+template <class T1>
+void output(T1 sample)
+{
+    while (!sample.empty())
+    {
+        std::cout << sample.top() << " || ";
+        sample.pop();
+    }
+}
 
 int main()
 {
+    //1
+    std::stack<my_time> sample1;
+    std::ifstream is("Files/for1");
+    if (!is.is_open())
+    {
+        std::cout << "Can't open the file!\n";
+        exit(0);
+    }
+    else
+    {
+        for(my_time d; is >> d;)
+            sample1.emplace(d);
+    }
+    is.close();
+    //2
+    sort_decrease(sample1);
+    //3
+    std::cout << "Container A: ";
+    output(sample1);
+    //4
 
     return 0;
 }
